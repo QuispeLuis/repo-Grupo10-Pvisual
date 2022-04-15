e122883855b3744d3a422912040586b5fb4c2be7
fv322 <38471258@fi.unju.edu.ar>
2022-04-15 20:47:02 -0300

    Creación y desarrollo del paquete Bisiesto para actividad 2.

---
+++ TP2_Grupo10/src/main/java/ar/edu/unju/fi/Bisiesto/Principal.java
@@ -0,0 +1,32 @@
+package ar.edu.unju.fi.Bisiesto;
+
+import java.util.InputMismatchException;
+import java.util.Scanner;
+
+public class Principal {
+
+	public static void main(String[] args) {
+		
+		Scanner entrada;
+		int año;
+		System.out.println("DETERMINAR SI EL AÑO INGRESADO ES BISIESTO.");
+		try {
+			System.out.println("Ingrese año: ");
+			entrada = new Scanner(System.in);
+			año = entrada.nextInt();
+
+			if (año % 4 == 0 && (año % 100 != 0 || año % 400 == 0)) {
+				System.out.println("El año " + año + " es bisiesto");
+			} else {
+				System.out.println("El año " + año + " no es bisiesto");
+			}
+			entrada.close();
+		} catch (InputMismatchException e) {
+			System.out.println("Error! ");
+		}
+
+		System.out.println("FIN DEL PROGRAMA.");
+
+	}
+
+}
